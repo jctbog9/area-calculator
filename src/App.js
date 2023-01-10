0a99e1ff791772c0994e5518e23980e3b80fb610
John Trocchi <jtrocchi1@gmail.com>
2023-01-10 00:13:49 -0500

    git init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
 
 function App() {
+  const [area, setArea] = useState(0)
+  const [length, setLength] = useState(0)
+  const [width, setWidth] = useState(0)
+
+  const handleLengthChange = (event) => {
+    setLength(event.target.value)
+  }
+  
+  const handleWidthChange = (event) => {
+    // Set the width here
+  }
+
+  const calculateArea = () => {
+    // This function should utilize the length and width values in state and calculate the area of a 4 sided figure.
+    // Your code goes here
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <h1>Area: {area}</h1>
+      <Form>
+        <Form.Group>
+          <Form.Label>Length</Form.Label>
+          <Form.Control type="number" onChange={handleLengthChange} value={length}></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Width</Form.Label>
+          <Form.Control type="number" onChange={handleWidthChange} value={width}></Form.Control>
+        </Form.Group>
+        <Button variant="primary" onClick={calculateArea}>
+          Calculate Area
+        </Button>
+      </Form>
+    </Container>
   );
 }
 
